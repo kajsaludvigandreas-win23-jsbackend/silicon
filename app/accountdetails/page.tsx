7a16cb9ab195ee91adca929d6e756477e952bd01
Ludvig <ludvig.vinoy@hotmail.com>
2024-05-03 23:23:10 +0200

    updated

--- app/accountdetails/page.tsx
+++ app/accountdetails/page.tsx
@@ -1,46 +1,46 @@
 import Link from 'next/link';
-import styles from 'AccountDetails.module.css'
+import styles from './AccountDetails.module.css'
 import AccountNav from '../components/accountSideNav/accountSideNav';
 
 export default function AccountDetails() {
     return (
         <section id="account">
-            <div className='container'>
+            <div className={`container ${styles.container}`}>
             <AccountNav/>
-            <div className='accountDetails'>
+            <div className={styles.accountDetails}>
                 <h1>Details</h1>
-                <form className='basic' action='/accountdetails'method='post'>
+                <form className={styles.basic} action='/accountdetails'method='post'>
                 <h3>Basic Info</h3>
-                <div className='basicContent'>
-                    <div id='basicFirstName' className='basicGroup'>
+                <div className={styles.basicContent}>
+                    <div id='basicFirstName' className={styles.basicGroup}>
                         <label htmlFor="firstName">First Name</label>
-                        <input className="input" type="text" id="firstName" />
+                        <input className={styles.input} type="text" id="firstName" />
                     </div>
 
-                    <div id='basicLastName' className='basicGroup'>
+                    <div id='basicLastName' className={styles.basicGroup}>
                         <label htmlFor="lastName">Last Name</label>
-                        <input className="input" type="text" id="lastName" />
+                        <input className={styles.input} type="text" id="lastName" />
                     </div>
 
-                    <div id="basicEmail" className= 'basicGroup'>
+                    <div id="basicEmail" className= {styles.basicGroup}>
                         <label htmlFor="email">Email</label>
-                        <input className="input" type="email" id="email" />
+                        <input className={styles.input} type="email" id="email" />
                     </div>
 
-                    <div id='basicPhone' className='basicGroup'>
+                    <div id='basicPhone' className={styles.basicGroup}>
                         <label htmlFor="phone">Phone Number</label>
-                        <input className='input' type="text" id='phone' />
+                        <input className={styles.input} type="text" id='phone' />
                     </div>
 
-                    <div id="basicBio" className="basicGroup">
-                        <div className="opt">
+                    <div id="basicBio" className={styles.basicGroup}>
+                        <div className={styles.opt}>
                         <label htmlFor='bio'>Bio</label>
-                        <p className="bio"> (optional)</p>
+                        <p className={styles.bio}> (optional)</p>
                         </div>
-                        <textarea className="bioInput"></textarea>
+                        <textarea className={styles.bioInput}></textarea>
                         </div>
                 </div>
-                <div className="basic-buttons">
+                <div className={styles.basicButtons}>
                     <button className="btn-gray" type="reset">Cancel</button>
                     <button className="btn-theme" type="submit">Save Changes</button>
                 </div>
