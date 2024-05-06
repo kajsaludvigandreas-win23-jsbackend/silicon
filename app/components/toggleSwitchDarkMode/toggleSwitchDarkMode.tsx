f816d8317087079630b53c65cf6b8d12632b9770
Andreas Fredriksson <afredriksson89@gmail.com>
2024-05-06 20:19:30 +0200

    New switch dark mode

--- app/components/toggleSwitchDarkMode/toggleSwitchDarkMode.tsx
+++ app/components/toggleSwitchDarkMode/toggleSwitchDarkMode.tsx
@@ -1,9 +1,10 @@
 import styles from './toggleSwitchDarkMode.module.css'
 
 
-export default function ToggleSwitchDarkMode() {
+
+export default function ToggleSwitchDarkMode() {  
     return ( 
-        <div className={styles.toggleSwitchDark}>
+        <div className={styles.toggleSwitchDark} >
             <input type="checkbox" id="switchDark" className={styles.checkbox}></input>
             <label htmlFor="switchDark" className={styles.toggle}>
             </label>
